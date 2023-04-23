e2a87f3f92af750432db986989d3be364bd9937d
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-04-23 00:17:15 +0000

    removed p tag

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,9 +8,7 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
 			
-			</p>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
