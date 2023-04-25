307e65854d5acdf9e6d291adc92b6761dd736f4f
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-04-25 01:04:13 +0000

    update lists using fetch through  api

--- src/js/component/todo.js
+++ src/js/component/todo.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 export default function Todo() {
 
   // list is stored in use state
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState("");
   const [displayx, setDisplayx] = useState(true);
   const [newTodo, setNewTodo] = useState([
     "do something",
@@ -13,21 +13,9 @@ export default function Todo() {
 
   // API call 1. save the todo list items in console 2 use console log to access the text array.label.... 3 make it show up on to do list replace do something etc
   let PostUrl = 'https://assets.breatheco.de/apis/fake/todos/user/mikiemapo';
-  useEffect (()=> {
-    fetch(PostUrl)
-      .then(
-        (recived) => {
-          return recived.json()
-        }
-      )
-      .then(
-        (represent) => {
-          console.log(represent, "ÏAMHER")
-        setNewTodo(represent)
-        }
-      )
-      .catch((error) => console.error(error))
-  },[])
+  useEffect(() => {
+    getTodo();
+  }, [])
 
   // useEffect(() => {
   //   GetTodoItem();
@@ -44,36 +32,51 @@ export default function Todo() {
 
 
   // map is used to output the template in our case its the do something array
-  let todoI = newTodo.map((item, index) => {
-    return (
-      // on mouse enter is the same as hover key property keeps track of each unique item is allways preceeded by map
-      <li
-        onMouseEnter={() => setDisplayx(false)}
-        onMouseLeave={() => setDisplayx(true)}
-        className="todoItem d-flex mx-3"
-        key={index}
-        
-      >
-        {item}
-        <div
-          className={displayx ? "d-none" : "remove"}
-          // remove task index reffering the function up top
-          onClick={() => {
-            removeTask(index);
-          }}
-        >
-          <span className="hidden">X</span>
-         
-        </div>
-      </li>
-    );
-  });
+  
   // add task to the list and . . . is a spread opp spread opp is used to allow itarables to be spread individually.
-  const addTodo = (onKeyDownEvent) => {
-    if (onKeyDownEvent.key === "Enter") {
-      const newTask = [...newTodo, task];
-      setNewTodo(newTask);
-      setTask("");
+  const getTodo = () => {
+    fetch(PostUrl)
+      .then(
+        (recived) => {
+          return recived.json()
+
+        }
+      )
+      .then(
+        (represent) => {
+          console.log(represent, "ÏAMHERE")
+          setNewTodo(represent)
+        }
+      )
+      .catch((error) => console.error(error))
+  }
+
+
+  const addTodo = (e) => {
+    if (e.key === "Enter") {
+      const newTask = [...newTodo, {label:task,done:false}];
+      console.log(newTask)
+      fetch('https://assets.breatheco.de/apis/fake/todos/user/mikiemapo', {
+        method: "PUT",
+        body: JSON.stringify(newTask),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(resp => {
+          console.log(resp.ok); // will be true if the response is successfull
+          console.log(resp.status); // the status code = 200 or code = 400 etc.
+          console.log(resp.text()); // will try return the exact result as string
+          //return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+        })
+        .then(data => {
+          getTodo()
+      })
+
+        .catch(error => {
+          //error handling
+          console.log(error);
+        });
     }
   };
 
@@ -87,11 +90,34 @@ export default function Todo() {
           value={task}
           onChange={(event) => setTask(event.target.value)}
           type="text"
-          onKeyDown={addTodo}
+          onKeyDown={(e)=> addTodo(e)}
         />
       </div>
-      <ul className="RemoveBullet">{todoI}</ul>
-      <div>{todoI.length} task left</div>
+      <ul className="RemoveBullet">{newTodo.map((item, index) => {
+    return (
+      // on mouse enter is the same as hover key property keeps track of each unique item is allways preceeded by map
+      <li
+        onMouseEnter={() => setDisplayx(false)}
+        onMouseLeave={() => setDisplayx(true)}
+        className="todoItem d-flex mx-3"
+        key={index}
+
+      >
+        {item.label}
+        <div
+          className={displayx ? "d-none" : "remove"}
+          // remove task index reffering the function up top
+          onClick={() => {
+            removeTask(index);
+          }}
+        >
+          <span className="hidden">X</span>
+
+        </div>
+      </li>
+    );
+  })}</ul>
+      <div>{newTodo.length} task left</div>
     </div>
   );
 }
---
+++ src/js/component/todo2.js
@@ -0,0 +1,19 @@
+// const getData = () => {
+//     axios
+//     .get('https://assets.breatheco.de/apis/fake/todos/user/username’)
+//     .then((response) => {
+//         console.log(response.data);
+//         setTodos(response.data);
+//     }, (error) => console.log(error.message))
+// }
+
+// const addData = () => {
+//     try{
+//         console.log('todos', todos);
+//         const response = axios.put('https://assets.breatheco.de/apis/fake/todos/user/username’, todos)
+//         .then(console.log(response.config.data));
+//     }
+//     catch(error){
+//         console.log(error.message)
+//     }
+// }
